a4ca0a7a64bc1d147564b5d9c16b300148e5f8f2
Chris Watnee <chriswatnee@users.noreply.github.com>
2026-01-08 15:40:25 -0500

    Set routes

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ const app = express();
 // Middleware setup
 app.set('view engine', 'pug');
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
+// Routes
+// Index route
+app.get('/', (req, res) => {
+  // Render homepage with project data
+  res.render('index', { projects });
+});
+
+// About route
+app.get('/about', (req, res) => {
+  // Render about page
+  res.render('about');
+});
+
+// Dynamic project route
+app.get('/project/:id', (req, res) => {
+  // Render a specific project based on ID
+  const { id } = req.params;
+  const project = projects.find(project => project.id === parseInt(id));
+  res.render('project', { project });
+});
