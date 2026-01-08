5a96502d912e0ae4e7762b077e41ae7917fc8e7a
Chris Watnee <chriswatnee@users.noreply.github.com>
2026-01-08 14:13:34 -0500

    Set up middleware

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const path = require('path');
 const { projects } = require('./data.json')
 
 const app = express();
+
+// Middleware setup
+app.set('view engine', 'pug');
+app.use('/static', express.static(path.join(__dirname, 'public')));
